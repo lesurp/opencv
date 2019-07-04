46d5eb5100ab7aa9db1aa9ebed6a04ba108e5d2a
Paul Lesur <paul.lesur@dfki.de>
2019-07-04 16:47:25 +0200

    improve speed of HarrisResonse computation
    20% increase in speed on my benchmarks

--- modules/features2d/src/orb.cpp
+++ modules/features2d/src/orb.cpp
@@ -142,30 +142,31 @@ HarrisResponses(const Mat& img, const std::vector<Rect>& layerinfo,
     float scale = 1.f/((1 << 2) * blockSize * 255.f);
     float scale_sq_sq = scale * scale * scale * scale;
 
-    AutoBuffer<int> ofsbuf(blockSize*blockSize);
-    int* ofs = ofsbuf.data();
-    for( int i = 0; i < blockSize; i++ )
-        for( int j = 0; j < blockSize; j++ )
-            ofs[i*blockSize + j] = (int)(i*step + j);
-
     for( ptidx = 0; ptidx < ptsize; ptidx++ )
     {
         int x0 = cvRound(pts[ptidx].pt.x);
         int y0 = cvRound(pts[ptidx].pt.y);
         int z = pts[ptidx].octave;
 
-        const uchar* ptr0 = ptr00 + (y0 - r + layerinfo[z].y)*step + x0 - r + layerinfo[z].x;
+        const uchar* ptr = ptr00 + (y0 - r + layerinfo[z].y)*step + x0 - r + layerinfo[z].x;
         int a = 0, b = 0, c = 0;
+        int offset = step - blockSize;
 
-        for( int k = 0; k < blockSize*blockSize; k++ )
+        for (int i = 0; i < blockSize; i++)
         {
-            const uchar* ptr = ptr0 + ofs[k];
-            int Ix = (ptr[1] - ptr[-1])*2 + (ptr[-step+1] - ptr[-step-1]) + (ptr[step+1] - ptr[step-1]);
-            int Iy = (ptr[step] - ptr[-step])*2 + (ptr[step-1] - ptr[-step-1]) + (ptr[step+1] - ptr[-step+1]);
-            a += Ix*Ix;
-            b += Iy*Iy;
-            c += Ix*Iy;
+            for (int j = 0; j < blockSize; j++)
+            {
+                int Ix = (ptr[1] - ptr[-1]) * 2 + (ptr[-step + 1] - ptr[-step - 1]) + (ptr[step + 1] - ptr[step - 1]);
+                int Iy = (ptr[step] - ptr[-step]) * 2 + (ptr[step - 1] - ptr[-step - 1]) + (ptr[step + 1] - ptr[-step + 1]);
+
+                a += Ix * Ix;
+                b += Iy * Iy;
+                c += Ix * Iy;
+                ++ptr;
+            }
+            ptr += offset;
         }
+
         pts[ptidx].response = ((float)a * b - (float)c * c -
                                harris_k * ((float)a + b) * ((float)a + b))*scale_sq_sq;
     }
